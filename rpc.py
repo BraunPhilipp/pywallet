0c84ef9e009f53f1f86fa68534910b0e42ab2677
Philipp Braun <3589810@gmail.com>
2021-08-01 00:30:54 +0200

    Zcash rpc update

--- rpc.py
+++ rpc.py
@@ -3,19 +3,35 @@ import requests
 
 rpc_user="zecwallet"
 rpc_password="cd3u0td7d5t"
+url = "http://127.0.0.1:18232/"
 
 # curl --user zecwallet --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getaddressbalance", "params": [{"addresses": ["tmYXBYJj1K7vhejSec5osXK2QsGa5MTisUQ"]}] }' -H 'content-type: text/plain;' http://127.0.0.1:18232/
 
 
-url = "http://127.0.0.1:18232/"
-payload = json.dumps({"method": "getaddressbalance", "params": [{"addresses": ["t1KLUV7nQE7htr8q9hjxbtB5zhzYZYCEPbK"]}]})
-headers = {'content-type': "application/json", 'cache-control': "no-cache"}
+zecf = open("./wallets/zcash-watch.csv", "r")
+pubs = zecf.readlines()
+pubs = [pub.split(",")[1].strip().replace("\n", "") for pub in pubs]
 
-response = requests.request("POST", url, data=payload, headers=headers, auth=(rpc_user, rpc_password))
-response = json.loads(response.text)
+total = 0
 
-print(response)
+for idx in range(0, len(pubs), 200):
+    pubsBatch = pubs[idx:idx+200]
 
+    # print(pubsBatch)
+
+    payload = json.dumps({"method": "getaddressbalance", "params": [{"addresses": pubsBatch}]})
+    headers = {'content-type': "application/json", 'cache-control': "no-cache"}
+
+    response = requests.request("POST", url, data=payload, headers=headers, auth=(rpc_user, rpc_password))
+    response = json.loads(response.text)
+
+    if response["result"] is not None:
+        print(response["result"])
+        total += float(response["result"]["received"]) / (10**8)
+
+    # print(response)
+
+print(total)
 
 # rpcuser=zecwallet
 # rpcpassword=cd3u0td7d5t
